4868c873ef02702a7642163e0b6d1d4fd4875b03
Devilal panchal <devilalpanchal551@gmail.com>
2024-03-07 18:46:37 +0530

    add

--- function.js
+++ function.js
@@ -1,62 +1,111 @@
 
 
 const inputGiveTask = document.querySelector('#input')
-console.log(inputGiveTask);
-
 const CreateTaskBtn = document.querySelector('.CreateBtn')
-console.log(CreateTaskBtn);
-
 const CreateTaskListContainer = document.querySelector('.createTask')
-// console.log(CreateTaskListContainer);
 const WantToClose = document.querySelector('.WantToClose')
-console.log(WantToClose);
 
 
-function AddNewTask(){
-const NewTask = document.createElement('div')
-NewTask.classList.add('OneMoreTask')
-NewTask.style.display = 'flex'
-NewTask.style.justifyContent = 'center'
-NewTask.style.alignItems = 'center'
-NewTask.style.gap = '70px'
-NewTask.style.width = '380px'
-NewTask.style.height = '50px'
-NewTask.style.border = '1px solid blue'
-NewTask.style.backgroundColor = 'wheat'
-NewTask.style.borderRadius = '20px'
-NewTask.style.marginTop = '20px'
-WantToClose.append(NewTask)
+// A function to exececute to click create task button
+CreateTaskBtn.addEventListener('click', AddNewTask)
 
-const Image = document.createElement('img')
-Image.src = 'check.png'
-Image.style.width = '20px'
-Image.style.height = '20px'
-NewTask.appendChild(Image)
+function AddNewTask() {
 
-const nameoftask = document.createElement('div')
-nameoftask.innerText = 'devilal panchal intro'
+        // inputGiveTask.forEach((item)=>{
+        //     item.addEventListener('keyup',(e)=>{
+        //         let input = e.target
+        //         if(e.keyCode == 13){
+        //             const NewTask = document.createElement('div')
+        //             NewTask.classList.add('OneMoreTask')
+        //             NewTask.style.display = 'flex'
+        //             NewTask.style.justifyContent = 'center'
+        //             NewTask.style.alignItems = 'center'
+        //             NewTask.style.gap = '70px'
+        //             NewTask.style.width = '380px'
+        //             NewTask.style.height = '50px'
+        //             NewTask.style.border = '1px solid blue'
+        //             NewTask.style.backgroundColor = 'wheat'
+        //             NewTask.style.borderRadius = '20px'
+        //             NewTask.style.marginTop = '20px'
+        //             WantToClose.append(NewTask)
+                
+        //             const Image = document.createElement('img')
+        //             Image.src = './image/uncheck.png'
+        //             Image.style.width = '20px'
+        //             Image.style.height = '20px'
+        //             NewTask.appendChild(Image)
+                
+                
+        //             const nameoftask = document.createElement('div')
+        //             nameoftask.innerText = inputGiveTask.value
+        //             NewTask.appendChild(nameoftask)
 
-NewTask.appendChild(nameoftask)
-
-const CloseTask = document.createElement('div')
-CloseTask.style.textAlign = 'center'
-CloseTask.style.border = '1px solid red'
-CloseTask.innerText = 'X'
-CloseTask.style.fontSize = '15px'
-CloseTask.style.width = '21px'
-CloseTask.style.height = '21px'
-CloseTask.style.color = 'red'
-NewTask.appendChild(CloseTask)
+        //         }
+        //     })
+        // })
 
 
+    const NewTask = document.createElement('div')
+    NewTask.classList.add('OneMoreTask')
+    NewTask.style.display = 'flex'
+    NewTask.style.justifyContent = 'center'
+    NewTask.style.alignItems = 'center'
+    NewTask.style.gap = '70px'
+    NewTask.style.width = '380px'
+    NewTask.style.height = '50px'
+    NewTask.style.border = '1px solid blue'
+    NewTask.style.backgroundColor = 'wheat'
+    NewTask.style.borderRadius = '20px'
+    NewTask.style.marginTop = '20px'
+    WantToClose.append(NewTask)
 
+    const Image = document.createElement('img')
+    Image.src = './image/uncheck.png'
+    Image.style.width = '20px'
+    Image.style.height = '20px'
+    NewTask.appendChild(Image)
+
+
+    const nameoftask = document.createElement('div')
+    nameoftask.innerText = inputGiveTask.value
+    NewTask.appendChild(nameoftask)
+
+
+
+    //********************** for done task add delete like cut**********************
+    Image.addEventListener('click', () => {
+        Image.src = './image/check.png'
+        Image.style.width = '20px'
+        Image.style.height = '20px'
+
+        alert('Task is completed')
+        confirm('You Can Not Reuse of this task')
+
+        const del = document.createElement('del')
+        del.textContent = inputGiveTask.value
+        nameoftask.innerText = inputGiveTask.value = '  '
+        nameoftask.append(del)
+
+
+    })
+
+
+//********************** */ to close task******************************
+    const CloseTask = document.createElement('div')
+    CloseTask.style.textAlign = 'center'
+    CloseTask.classList.add('closeContent')
+    CloseTask.style.border = '1px solid red'
+    CloseTask.innerText = 'X'
+    CloseTask.style.fontSize = '15px'
+    CloseTask.style.width = '21px'
+    CloseTask.style.height = '21px'
+    CloseTask.style.color = 'red'
+    NewTask.appendChild(CloseTask)
+
+    CloseTask.addEventListener('click', () => {
+        NewTask.style.display = 'None'
+    })
 
 }
 
 
-
-CreateTaskBtn.addEventListener('click',AddNewTask)
-
-CloseTask.addEventListener('click',()=>{
-    WantToClose.classList.add('hide')
-})
